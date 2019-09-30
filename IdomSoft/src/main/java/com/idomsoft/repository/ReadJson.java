f5581cb35dd35284dfe4e5a986de2a2803eacf38
sztyopu <vargasztyopu@gmail.com>
2019-09-30 21:49:54 +0200

    MAP-kod és allapolgar

--- IdomSoft/src/main/java/com/idomsoft/repository/ReadJson.java
+++ IdomSoft/src/main/java/com/idomsoft/repository/ReadJson.java
@@ -2,6 +2,7 @@ package com.idomsoft.repository;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -46,27 +47,36 @@ public class ReadJson implements CommandLineRunner {
             ObjectMapper objectMapper2 = new ObjectMapper();
             
 //          read json file (kodszotar21_allampolg.json) and convert to Allampolgarsag object 
-        	Allampolgarsag[] allampolgarsag = objectMapper2.readValue(new File("src/main/resources/static/JSON/kodszotar21_allampolg.json"), Allampolgarsag[].class);
-/*            System.out.println("JSON array to Array objects...");
-            for (Allampolgarsag allam : allampolgarsag) {
-                System.out.println(allam);
-            }
-*/           
+//        	Allampolgarsag[] allampolgarsag = objectMapper2.readValue(new File("src/main/resources/static/JSON/kodszotar21_allampolg.json"), Allampolgarsag[].class);
+//            System.out.println("JSON array to Array objects...");
+//            for (Allampolgarsag allam : allampolgarsag) {
+//                System.out.println(allam);
+//            }
+     
         	
 //          read json file (kodszotar21_allampolg.json) and convert to List      	
         	List<Allampolgarsag> allampolgarsagList = objectMapper2.readValue(new File("src/main/resources/static/JSON/kodszotar21_allampolg.json"), new TypeReference<List<Allampolgarsag>>(){});
 /*        	System.out.println("\nJSON array to List of objects");
         	allampolgarsagList.stream().forEach(x -> System.out.println(x));
 */        	
- //       	System.out.println(allampolgarsagList.get(0));
-        
+         
         	//Vissza adja azt az objektumot ahol a kod:"HUN"
-        	List<Allampolgarsag> result = allampolgarsagList.stream()
-        		     .filter(item -> item.getKod().equals("HUN"))
-        		     .collect(Collectors.toList());
-        	System.out.println("result: " +result);
-        	//nem tudom, hogy ezt hogy keressem meg?!?!?!
-        	System.out.println("result allampolgarsag: " );
+//        	List<Allampolgarsag> result = allampolgarsagList.stream()
+//        		     .filter(item -> item.getKod().equals("HUN"))
+//        		     .collect(Collectors.toList());
+        	
+        	//Stringlistába rakja az "allampolgarsag"és a "kod" valtozok ertekeit
+            List<String> collect1 = allampolgarsagList.stream().map(x -> x.getAllampolgarsag()).collect(Collectors.toList());
+        	List<String> collect2 = allampolgarsagList.stream().map(x -> x.getKod()).collect(Collectors.toList());
+        	
+        	//HashMab-be rakom a két String listát a konnyu kereshetoseg veget (ahol a kod-key, allampolgarsag-value)
+            HashMap<String,String> hmap = new HashMap<>();
+            for (int i = 0; i < collect1.size();  i++) {
+            	hmap.put(collect2.get(i),collect1.get(i));
+            	}
+            System.out.println(hmap);
+            
+      
         } catch (Exception e) {
         	System.out.println(e);
           }
