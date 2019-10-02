52fa4398e4547a26c313af389bd2745972681337
sztyopu <vargasztyopu@gmail.com>
2019-10-02 13:32:23 +0200

    sikerult tesztelni a 2. microservicet

--- IdomSoft/src/main/java/com/idomsoft/controller/ApiController.java
+++ IdomSoft/src/main/java/com/idomsoft/controller/ApiController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.idomsoft.DTO.OkmanyDTO;
 import com.idomsoft.DTO.OkmanyServiceResp;
@@ -32,22 +33,26 @@ public class ApiController {
     	
     	// itt meghíjuk a SemelyService-ben megírt validációs metódust, ami visszad egy hiba listát 
     	List<String> errorList = szemelyServiece.szemelyServiceValidacio(szemelyDTO);
-        
+        // 2. microservice meghivása
+    	final String uri = "http://localhost:8080/okmany";
+    	RestTemplate restTemplate = new RestTemplate();
+    	OkmanyServiceResp okmanyServiceResp = restTemplate.getForObject(uri, OkmanyServiceResp.class);
+    	
     	szemelyServiceResp.setLista(errorList);
     	szemelyServiceResp.setSzemely(szemelyDTO);
     	return szemelyServiceResp;
     }
 
     @RequestMapping(value = "/okmany", method = RequestMethod.POST)
-    public OkmanyServiceResp okmanyValidation(@RequestBody ArrayList<OkmanyDTO> okmLista) {
+    public OkmanyServiceResp okmanyValidation(@RequestBody SzemelyDTO szemelyDTO) {
     	 //okmanyServiceResp egyszerre tudja tárolni a módosított okmLista-t és a hiba listát
     	OkmanyServiceResp okmanyServiceResp = new OkmanyServiceResp();
     	
     	// itt meghíjuk a OkmanyService-ben megírt validációs metódust, ami visszad egy hiba listát 
-    	ArrayList<String> errorsOkmanyLista = okmanyServiece.okmanyServiceValidacio(okmLista);
+    	ArrayList<String> errorsOkmanyLista = okmanyServiece.okmanyServiceValidacio(szemelyDTO.getOkmLista());
     	
     	okmanyServiceResp.setErrorsOkmanyLista(errorsOkmanyLista);
-    	okmanyServiceResp.setOkmLista(okmLista);
+    	okmanyServiceResp.setOkmLista(szemelyDTO.getOkmLista());
         
         return okmanyServiceResp;
     }
