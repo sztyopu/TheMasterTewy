5b444638b611187b6a39c5536d95a3b6a3bb087a
Sztyopu <vargasztyopu@gmail.com>
2019-11-08 22:11:05 +0100

    try + err msg

--- money_shiwaforce/src/com/company/Main.java
+++ money_shiwaforce/src/com/company/Main.java
@@ -6,19 +6,24 @@ public class Main {
 /*
 Készitsen egy olyan modult amely egy rendszer részeként visszajáró összeget számol a lehető legkevesebb darabszámú
 pénjegy és érme (magyar forint) felhasználásával. A bemenő paraméterek a fizetendő és a fizetett összeg.
-Figyeljen, hoyg a lehető legteljesebb megoldást adja!
+Figyeljen, hogy a lehető legteljesebb megoldást adja!
 */
     public static void main(String[] args) {
+    try{
         Scanner scan = new Scanner(System.in);
-            System.out.print("The amount due: ");
-            int amount = scan.nextInt();
-            System.out.print("The amount paid: ");
-            int paid = scan.nextInt();
+        System.out.print("The amount due: ");
+        int amount = scan.nextInt();
+        System.out.print("The amount paid: ");
+        int paid = scan.nextInt();
 /*
        int paid = 100000;
        int amount = 15000;
 */
         System.out.println("Your change is: " + (paid-amount) + " Ft = " + moneyBack(amount, paid));
+    }
+    catch (Exception e){
+        System.err.println("A formátum nem megfelelő! Csak egész számot adhat meg!");
+    }
     }
 
     public static StringBuilder moneyBack(int amount, int paid){
