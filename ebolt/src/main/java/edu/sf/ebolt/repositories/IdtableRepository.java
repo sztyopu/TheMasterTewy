95346e1241564176c307b740555535a3648f362a
Sztyopu <vargasztyopu@gmail.com>
2020-02-18 16:50:34 +0100

    deleted

--- ebolt/src/main/java/edu/sf/ebolt/repositories/IdtableRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.sf.ebolt.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.sf.ebolt.entities.Idtable;
-
-public interface IdtableRepository extends JpaRepository<Idtable, String> {
-
-}
